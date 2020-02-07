da7794a94baf2fe1f3e0513ef0170b4f3d3a47d6
AlessandraRuiz03 <183416@ids.upchiapas.edu.mx>
2020-02-07 17:09:38 -0600

    third

--- CS/settings.py
+++ CS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '17+fro1vh8rgs^sygd)2fp1k67kype=0rhwx0vr%q82x%f=5d%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','3.84.117.193','127.0.0.1']
+ALLOWED_HOSTS = ['localhost','54.173.173.192','127.0.0.1']
 
 
 
