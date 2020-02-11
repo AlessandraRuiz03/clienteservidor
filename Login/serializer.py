39d5b601cc3a2894942f23ee2c987834e105704f
AlessandraRuiz03 <183416@ids.upchiapas.edu.mx>
2020-02-10 19:10:47 -0600

    actualizado3

---
+++ Login/serializer.py
@@ -0,0 +1,11 @@
+# -- agregando librerias framework
+from rest_framework import routers , serializers, viewsets
+
+#----agregando modelos
+
+from Login.models import Example2
+
+class Example2Serializers (serializers.ModelSerializer):
+    class Meta:
+        model = Example2
+        fields = ('__all__')
